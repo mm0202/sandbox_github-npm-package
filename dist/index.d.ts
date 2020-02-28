fbfc8b78659010cdd52775d7e72a9ee63ddf94ef
m <for_etc@hotmail.com>
2020-02-28 13:33:55 +0900

    ts trial

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,12 @@
-export { default } from "./ts/FirestoreEmulatorProvider";
+import * as firebase from "@firebase/testing";
+export declare class FirestoreEmulatorProvider {
+    private projectId;
+    private readonly rules;
+    constructor(projectId: string, rulesFilePath?: string);
+    protected getProjectID(): string;
+    loadRules(): Promise<void>;
+    getFirestore(): firebase.firestore.Firestore;
+    getFirestoreWithAuth(uid?: string, email?: string): firebase.firestore.Firestore;
+    getAdminFirestore(): firebase.firestore.Firestore;
+    cleanup(): Promise<any[]>;
+}
