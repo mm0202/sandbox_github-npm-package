a36e2dc5012ba9e7248624447186d9c72b856cd4
m <for_etc@hotmail.com>
2020-02-28 13:18:17 +0900

    add dist dir

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var FirestoreEmulatorProvider_1 = require("./ts/FirestoreEmulatorProvider");
+exports.default = FirestoreEmulatorProvider_1.default;
---
+++ dist/ts/FirestoreEmulatorProvider.js
@@ -0,0 +1,37 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const firebase = require("@firebase/testing");
+const fs = require("fs");
+class FirestoreEmulatorProvider {
+    constructor(projectId, rulesFilePath = 'firestore.rules') {
+        this.projectId = projectId;
+        this.rules = fs.readFileSync(rulesFilePath, 'utf8');
+    }
+    getProjectID() {
+        return this.projectId;
+    }
+    async loadRules() {
+        return firebase.loadFirestoreRules({
+            projectId: this.getProjectID(),
+            rules: this.rules,
+        });
+    }
+    getFirestore() {
+        return firebase.initializeTestApp({
+            projectId: this.getProjectID(),
+        }).firestore();
+    }
+    getFirestoreWithAuth(uid = "test_user", email = "test_user@dummy.mail") {
+        return firebase.initializeTestApp({
+            projectId: this.getProjectID(),
+            auth: { uid: uid, email: email },
+        }).firestore();
+    }
+    getAdminFirestore() {
+        return firebase.initializeAdminApp({ projectId: this.getProjectID() }).firestore();
+    }
+    async cleanup() {
+        return Promise.all(firebase.apps().map(app => app.delete()));
+    }
+}
+exports.default = FirestoreEmulatorProvider;
